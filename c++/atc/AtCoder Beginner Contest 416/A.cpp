86101b7062ad5a7f11d0dbe555fff7ab4dd3b5ee
abovethe-clouds <2325598963@qq.com>
2025-07-26 22:07:17 +0800

    sel

---
+++ c++/atc/AtCoder Beginner Contest 416/A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+
+int read()
+{
+    int x = 0, w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9')
+    {  
+        if (ch == '-') w = -1;        
+        ch = getchar();               
+    }
+    while (ch >= '0' && ch <= '9')
+    {  
+        x = x * 10 + (ch - '0');
+        ch = getchar();  
+    }
+    return x * w; 
+}
+
+void solve()
+{
+    int n, l, r;
+    cin >> n >> l >> r;
+    string s;
+    cin >> s;
+    l--, r--;
+    for (int i = l; i <= r; i++)
+    {
+        if(s[i]!='o')
+        {
+            cout << "No";
+            return;
+        }
+    }
+    cout << "Yes" << endl;
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE 
+        //freopen("test.in", "r", stdin);
+       // freopen("test.out", "w", stdout);
+    #endif
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
+
+    return 0;
+}
