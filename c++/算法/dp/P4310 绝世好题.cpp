ea6d50eae52f9b036792ec8a8dcff069d67ad54d
abovethe-clouds <2325598963@qq.com>
2025-07-28 23:36:42 +0800

    sel

---
+++ c++/算法/dp/P4310 绝世好题.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+
+int read()
+{
+    int x = 0, w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            w = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = x * 10 + (ch - '0');
+        ch = getchar();
+    }
+    return x * w;
+}
+
+void solve()
+{
+    int n = read(), a[32] = {};
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        int no = 0;
+        v[i] = read();
+        for (int j = 0; j < 32; j++)
+        {
+            if (v[i] >> j & 1)
+                no = max(no, a[j] + 1);
+        }
+        for (int j = 0; j < 32; j++)
+        {
+            if (v[i] >> j & 1)
+                a[j] = max(a[j], no);
+        }
+    }
+    int maxx = 0;
+    for (int i = 0; i < 32; i++)
+        maxx = max(maxx, a[i]);
+    cout << maxx << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("test.in", "r", stdin);
+    //     freopen("test.out", "w", stdout);
+    // #endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
