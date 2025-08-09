22ee78172e663bdccd662b6a02e9b2730caeaa24
abovethe-clouds <2325598963@qq.com>
2025-08-09 19:48:41 +0800

    sel

---
+++ c++/cf/Educational Codeforces Round 13/B.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+
+int read()
+{
+    int x = 0, w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-') w = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = x * 10 + (ch - '0');
+        ch = getchar();
+    }
+    return x * w;
+}
+
+void solve()
+{
+    int n = read();
+    bool judge = (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0));
+    int ans = 0;
+    while (1)
+    {
+        n++;
+        if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))
+            ans = (ans+366) % 7;
+        else
+            ans = (ans+365) % 7;
+        if (ans==0&& judge==(n % 400 == 0 || (n % 4 == 0 && n % 100 != 0)))
+            break;
+
+    }
+    cout<<n<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
---
+++ c++/cf/Educational Codeforces Round 13/C.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+
+int read()
+{
+    int x = 0, w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-') w = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = x * 10 + (ch - '0');
+        ch = getchar();
+    }
+    return x * w;
+}
+
+void solve()
+{
+    ll n,a,b,p,q;
+    cin>>n>>a>>b>>p>>q;
+    ll lcm=a/__gcd(a,b)*b;
+
+    ll ans3=n/lcm;
+    ll ans1=n/a-ans3;
+    ll ans2=n/b-ans3;
+    cout<<ans1*p+ans2*q+ans3*max(q,p)<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
