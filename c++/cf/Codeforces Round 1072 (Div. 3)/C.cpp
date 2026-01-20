61ea69848ba4050bac3e59bfe026b30df87c573e
abovethe-clouds <2325598963@qq.com>
2026-01-20 10:49:01 +0800

    feat: add multiple new problem-solving implementations

---
+++ c++/cf/Codeforces Round 1072 (Div. 3)/C.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+#define int ll
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+int read()
+{
+    int x = 0, w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-') w = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = x * 10 + (ch - '0');
+        ch = getchar();
+    }
+    return x * w;
+}
+
+void solve()
+{
+    int n=read(),m=read();
+    queue<pii> q;
+    q.emplace(n,0);
+    set<int> vis;
+    vis.emplace(n);
+    while (!q.empty())
+    {
+        auto [x,step]=q.front();
+        q.pop();
+        if(x==m)
+        {
+            cout<<step<<endl;
+            return;
+        }
+        if(x<m)
+        {
+            cout<<-1<<endl;
+            return;
+        } else
+        {
+            int a=x/2,b=x-a;
+            if (!vis.count(b))
+            {
+                vis.emplace(b);
+                q.emplace(b,step+1);
+            }
+            if (!vis.count(a))
+            {
+                vis.emplace(a);
+                q.emplace(a,step+1);
+            }
+
+
+        }
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+#endif
+    int t = 1;
+    t = read();
+    while (t--)
+        solve();
+    return 0;
+}
