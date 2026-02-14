45552d70f7e32a6669ba27de484a9ba78c745eb0
abovethe-clouds <2325598963@qq.com>
2026-02-15 01:26:35 +0800

    refactor: clean up code and add new Minesweeper game implementation

--- c++/算法/字符串/P5357 【模板】AC 自动机.cpp
+++ c++/算法/字符串/P5357 【模板】AC 自动机.cpp
@@ -1,61 +1,109 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=2e5+5;
-int cnt=1;
-int trie[maxn][26],fail[maxn];
-void instert(string s)
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131,maxn=2e5+10;
+int alert[maxn],tree[maxn][26],fail[maxn],cnt=0,times[maxn];
+vector<int> mp[maxn];
+void insert(int i,string s)//插入字符串
 {
-    int cur=1;//根节点
+    int cnr=0;
+    for(auto c:s)
+    {
+        int to=c-'a';
+        if(!tree[cnr][to])
+            tree[cnr][to]=++cnt;
+        cnr=tree[cnr][to];
+    }
+    alert[i]=cnr;
+}
+void setFail()// 构建失败指针 建立直通表
+{
+    queue<int> q;
+    for(int i=0;i<26;i++)
+    {
+        if(tree[0][i])
+        {
+            fail[tree[0][i]]=0;
+            q.push(tree[0][i]);
+        }
+    }
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        for(int i=0;i<26;i++)
+        {
+            if(tree[u][i])
+            {
+                fail[tree[u][i]]=tree[fail[u]][i];
+                q.push(tree[u][i]);
+            }
+            else
+                tree[u][i]=tree[fail[u]][i];
+        }
+    }
+
+}
+
+void query(string s)//查询字符串
+{
+    int cur=0;
     for (auto i : s)
     {
-        int path=i-'a';
-        if(trie[cur][path]==0)
-        {
-            trie[cur][path]=++cnt;
-        }
-        cur=trie[cur][path];//下一个节点
+        cur=tree[cur][i-'a'];
+        times[cur]++;
     }
-    return;
 }
-void bfs()
+void addEdge(int u,int v)
 {
-    int cur=1;
-    queue<int> q;
-    for (int i = 0; i < 26; i++)
+    mp[u].push_back(v);
+}
+void dfs(int u)
+{
+    for(auto v:mp[u])
     {
-        if(trie[cur][i])
-        {
-            fail[trie[cur][i]]=1;
-            q.push(trie[cur][i]);
-        }
-    }
-    while (!q.empty())
-    {
-        int t=q.front();
-        q.pop();
-        for (int i = 0; i < 26; i++)
-        {
-
-        }
+        dfs(v);
+        times[u]+=times[v];
     }
 }
 void solve()
 {
     int n;
     cin>>n;
-    vector<string> a(n);
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-    string s;
-    cin>>s;
-    for(int i=0;i<n;i++)
-        instert(a[i]);
+    for (int i=1;i<=n;i++)
+    {
+        string s;
+        cin>>s;
+        insert(i,s);
+    }
+    setFail();
+    string text;
+    cin>>text;
+    query(text);
+    for (int i=1;i<=cnt;i++)
+    {
+        addEdge(fail[i],i);
+    }
+    dfs(0);
+    for (int i=1;i<=n;i++)
+    {
+        cout<<times[alert[i]]<<endl;
+    }
 }
+
 signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    solve();
+#ifndef ONLINE_JUDGE
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
     return 0;
-}
+}//
+// Created by Administrator on 2026/1/11.
+//
