60b1bdec66e43e0fa44326924c6abde33ba299f8
abovethe-clouds <2325598963@qq.com>
2026-07-18 19:16:58 +0800

    feat: add new problem-solving implementations in multiple files

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,12 @@
-#include <limits>
 #include<bits/stdc++.h>
 using namespace std;
 #define fir first
 #define sec second
 #define endl "\n"
 typedef long long ll;
-#define int long long
+#define int ll
 typedef unsigned long long ull;
-typedef pair<int, int> pii;
+typedef pair<int,int> pii;
 typedef pair<ll, ll> pll;
 const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
 int read()
@@ -29,26 +28,30 @@ int read()
 
 void solve()
 {
-    int n = read(),ansl=1,ansr=1,ans=0;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        a[i] = read();
-    for (int i=0; i<n; i++)
+    string s;
+    cin>>s;
+    stack<char> st;
+    for (auto i: s)
     {
-        int minn=0,maxx=0;
-        for (int j=i+1; j<n; j++)
+        if (i=='(')
+            st.push(i);
+        else
         {
-            if (a[j]>a[i]) maxx++;
-            else if (a[j]<a[i]) minn++;
-            if (minn-maxx>ans)
+            if (i==')')
             {
-                ans=minn-maxx;
-                ansl=i+1;
-                ansr=j+1;
+                if (st.empty()||st.top()!='(')
+                {
+                    cout<<"NO\n";
+                    return;
+                }
+                st.pop();
             }
         }
     }
-    cout<<ansl<<" "<<ansr<<endl;
+    if (st.empty())
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
 }
 
 signed main()
@@ -57,11 +60,11 @@ signed main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
-    // freopen("test.in", "r", stdin);
-    // freopen("test.out", "w", stdout);
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
 #endif
     int t = 1;
-    t = read();
+    //t = read();
     while (t--)
         solve();
     return 0;
