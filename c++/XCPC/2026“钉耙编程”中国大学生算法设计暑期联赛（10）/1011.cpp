a340adc800cd5adfdac8661b67a379c00f939884
abovethe-clouds <2325598963@qq.com>
2026-08-25 09:06:53 +0800

    add initial implementations for test input, developer tools, and multiple problem files

---
+++ c++/XCPC/2026“钉耙编程”中国大学生算法设计暑期联赛（10）/1011.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fir first
+#define sec second
+#define endl "\n"
+typedef long long ll;
+#define int ll
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7, inf = 0x3f3f3f3f, P = 131;
+
+void solve()
+{
+    vector<int> v={2, 17, 687};
+    int q;
+    cin>>q;
+    while (q--)
+    {
+        int x;
+        cin>>x;
+        auto it=lower_bound(v.begin(),v.end(),x);
+        if (it==v.end())
+            cout<<-1<<endl;
+        else
+            cout<<*it<<endl;
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w", stdout);
+#endif
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}//
+// Created by Administrator on 2026/8/20.
+//
